8052a439de50850eeb52dc4bdc19301f71d6964e
hsn-kamran <141766697+hsn-kamran@users.noreply.github.com>
2023-08-08 21:09:25 +0300

    Delete FiguresConsole directory
    deleting unnecessary folder

--- FiguresConsole/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Figures.Models;
-using Figures;
-
-Figure figure = new Triangle(3, 4, 5); 
-Figure figure2 = new Circle(5);
-
-
-Console.WriteLine(((Triangle)figure).RightAngled);
-Console.WriteLine(figure2.Area);
